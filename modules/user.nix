d3771b206f4ac23bf848181d5f9cbb463a0d1ccf
InsaneBroccoli <aware_lucite0l@icloud.com>
2026-08-11 14:17:05 +0200

    Add pin uid for the user

--- modules/user.nix
+++ modules/user.nix
@@ -4,6 +4,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${vars.username} = {
     isNormalUser = true;
+    uid = 1000;
     description = vars.username;
     extraGroups = ["networkmanager" "wheel" ]; # Enable ‘sudo’ for the user.
   };
