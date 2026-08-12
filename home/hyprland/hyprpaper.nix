9589f8748bac7935024fab0c9ab33aeb168faa3a
InsaneBroccoli <aware_lucite0l@icloud.com>
2026-08-13 00:39:29 +0200

    Change wallpaper

--- home/hyprland/hyprpaper.nix
+++ home/hyprland/hyprpaper.nix
@@ -6,11 +6,11 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = "~/Pictures/wallpapers/wallpaper.webp";
+      preload = "~/Pictures/wallpapers/alena-aenami-lights1k1.jpg";
       splash = false;
       wallpaper = {
         monitor="eDP-1";
-        path="~/Pictures/wallpapers/wallpaper.webp";
+        path="~/Pictures/wallpapers/alena-aenami-lights1k1.jpg";
       };
     };
   };
