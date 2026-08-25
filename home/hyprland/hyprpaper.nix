ba5d90564207d057b3df94673555ede98d9e9766
InsaneBroccoli <aware_lucite0l@icloud.com>
2026-08-25 14:22:34 +0200

    Fix hyprpaper path

--- home/hyprland/hyprpaper.nix
+++ home/hyprland/hyprpaper.nix
@@ -6,11 +6,11 @@
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = "../../pictures/wallpapers/alena-aenami-lights1k1.jpg";
+      preload = "~/nixos/pictures/wallpapers/alena-aenami-lights1k1.jpg";
       splash = false;
       wallpaper = {
         monitor = vars.monitor;
-        path = "../../pictures/wallpapers/alena-aenami-lights1k1.jpg";
+        path = "~/nixos/pictures/wallpapers/alena-aenami-lights1k1.jpg";
       };
     };
   };
