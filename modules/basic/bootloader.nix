76b0b5dd0fcb3272dca2a924c1dd4b4d98b428f3
InsaneBroccoli <aware_lucite0l@icloud.com>
2026-09-13 11:46:45 +0200

    Disable boot editor and add 4 new claude agents

--- modules/basic/bootloader.nix
+++ modules/basic/bootloader.nix
@@ -7,7 +7,10 @@
 
 {
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    editor = false;
+  };
   boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
 
