22ee537299b646d60c19b6a29d445c9b18e2bfc4
InsaneBroccoli <aware_lucite0l@icloud.com>
2026-09-13 11:56:03 +0200

    Add tpm2 pin

--- modules/encryption.nix
+++ modules/encryption.nix
@@ -1,9 +1,12 @@
 { ... }:
 
 {
-  # enroll tpm2: sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7 /dev/<luks-partition>
+  # enroll tpm2: sudo systemd-cryptenroll --tpm2-device=auto --tpm2-pcrs=7 --tpm2-with-pin=yes /dev/<luks-partition>
   # wipe:        sudo systemd-cryptenroll --wipe-slot=tpm2
   boot.initrd.systemd.enable = true;
-  boot.initrd.luks.devices."cryptroot".crypttabExtraOpts = [ "tpm2-device=auto" ];
+  boot.initrd.luks.devices."cryptroot".crypttabExtraOpts = [
+    "tpm2-device=auto"
+    "tpm2-pin=yes"
+  ];
   boot.initrd.luks.devices."cryptroot".bypassWorkqueues = true;
 }
